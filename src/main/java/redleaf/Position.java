f0ca6806f8db486351d2f92aa4b50eb38bbd7a41
wwadmin <wwadmin@langners>
2019-11-25 04:32:24 -0600

    Just a few more .gitignore files / changes and some missing artifacts from my work from the Langner laptop.

---
+++ src/main/java/redleaf/Position.java
@@ -0,0 +1,31 @@
+package redleaf;
+
+import java.time.Instant;
+
+import org.influxdb.annotation.Column;
+import org.influxdb.annotation.Measurement;
+import org.influxdb.annotation.TimeColumn;
+
+@Measurement(name="Position")
+public class Position {
+	
+	public Position() { }
+	
+	@Column(name="X")
+	private Long x;
+	public Long getX() { return x; }
+	
+	@Column(name="Y")
+	private Long y;
+	public Long getY() { return y; }
+	
+	@Column(name="Z")
+	private Long z;
+	public Long getZ() { return z; }
+	
+	@TimeColumn
+	@Column(name="time")
+	private Instant time;
+	public Instant getTime() { return time; }
+		
+}
