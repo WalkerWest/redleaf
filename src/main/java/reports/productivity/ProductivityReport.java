b6c2f13b2dd0627638db838883f6810cbedcf4ef
WalkerWest <walker.west@gmail.com>
2019-12-18 05:32:41 -0600

    Ahhh .... the fun continues; defined report variables for Q3, create the main report loop and calculated the first statistic (totalTime).

--- src/main/java/reports/productivity/ProductivityReport.java
+++ src/main/java/reports/productivity/ProductivityReport.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,7 +31,7 @@ import redleaf.Reportable;
 import redleaf.SpeedNForceV1;
 
 public class ProductivityReport implements Reportable {
-
+	
 	public String getName() { return "Q3: Productivity"; }
 	public String getReportFilename() { return null; }
 	public String getJrResourceStream() { return null; }
@@ -46,6 +47,12 @@ public class ProductivityReport implements Reportable {
 		influx.setDatabase(RLSingle.getInstance().getPrefs().getInfluxDb());
 	}
 	
+	private Integer reportDriver=15137;
+	public Integer getReportDriver() { return reportDriver; }
+	public void setReportDriver(Integer reportDriver) {
+		this.reportDriver = reportDriver;
+	}
+	
 	// 0.0 140.0 -30.0 250.0 0.0 3.8
 	float xLow=(float) 0.0,   xHigh=(float) 140.0, 
 		  yLow=(float) -30.0, yHigh=(float) 250.0, 
@@ -57,7 +64,8 @@ public class ProductivityReport implements Reportable {
 	private HashMap<String,String> palletMap = new HashMap<String,String>();
 	private HashMap<String,String> truckMap = new HashMap<String,String>();
 	private List<String> validDevs=new ArrayList<String>();
-	private HashMap<String,List<Position>> positions = new HashMap<String,List<Position>>();
+	private HashMap<String,List<Position>> positions = 
+			new HashMap<String,List<Position>>();
 	private HashMap<Integer,LinkedList<SpeedNForceV1>> 
 		drivers = new HashMap<Integer,LinkedList<SpeedNForceV1>>(); 
 	private HashMap<Integer,HashMap<String,List<Instant>>> 
@@ -71,7 +79,11 @@ public class ProductivityReport implements Reportable {
 		boolean debug=true;
 		reportPrep(debug);
 		
-		String queryStr="select \"Sonar Range\", \"vl53l1 Flags\", \"vl53l1 Range\", "
+		HashMap<String,List<PalletSensorV1>> rangeData = 
+				new HashMap<String,List<PalletSensorV1>>(); 
+		
+		String queryStr="select \"Sonar Range\", "
+				+ "\"vl53l1 Flags\", \"vl53l1 Range\", "
 				+ "Device from \"Pallet Sensor V1\" "
 				+ "where time >= "+start+" and time <= "+stop+" "
 				+ "group by Device";
@@ -80,27 +92,81 @@ public class ProductivityReport implements Reportable {
 		TimeUnit timeUnit = TimeUnit.NANOSECONDS;
 		QueryResult r = influx.query(query, timeUnit);
 		InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
-		List<PalletSensorV1> palletList=resultMapper.toPOJO(r, PalletSensorV1.class);
+		List<PalletSensorV1> palletList=
+				resultMapper.toPOJO(r, PalletSensorV1.class);
 		System.out.println("I found "+palletList.size()+" entries!");
 		List<String> hitList = new ArrayList<String>();
 		for(PalletSensorV1 pallet : palletList) {
-			if(!hitList.contains(pallet.getDevice())) {
-				System.out.print("Pallet sensor is "+pallet.getDevice());
-				System.out.println("; matching forklist is "+palletMap.get(pallet.getDevice().substring(1)));
-				hitList.add(pallet.getDevice());
+			String truck=palletMap.get(pallet.getDevice().substring(1));
+			if(truck==null)palletMap.get(pallet.getDevice());
+			if(truck!=null) {
+				if(!rangeData.containsKey(truck)) {
+					List<PalletSensorV1> myList = 
+							new LinkedList<PalletSensorV1>();
+					rangeData.put(truck, myList);
+				}
+				if(pallet.getVl53l1flags()==0) rangeData.get(truck).add(pallet);
 			}
 		}
-		//Gson gson = new Gson();
+		Gson gson = new Gson();
+		System.out.println("The rangeData is now "+gson.toJson(rangeData));
+		
+		Duration timeUnloaded = Duration.ofSeconds(0);
+		Float unloadedPercentage = new Float(0.0);
+		Duration timeLoaded = Duration.ofSeconds(0);
+		Float loadedPercentage = new Float(0.0);
+		Duration totalTime = Duration.ofSeconds(0);
+		List<Instant> unloadedTimeIntervals = new ArrayList<Instant>();
+		List<Instant> loadedTimeIntervals = new ArrayList<Instant>();
+		Double totalDistance = new Double(0.0);
+		Double unloadedDistance = new Double(0.0);
+		Double loadedDistance = new Double(0.0);
+		Duration totalTimeMoving = Duration.ofSeconds(0);
+		Duration timeMovingUnloaded = Duration.ofSeconds(0);
+		Duration timeMovingLoaded = Duration.ofSeconds(0);
+		Duration totalTimeSitting = Duration.ofSeconds(0);
+		Duration timeSittingUnloaded = Duration.ofSeconds(0);
+		Duration timeSittingLoaded = Duration.ofSeconds(0);
+		Boolean sensorDataFound=false;
+		List<String> forklifts = new ArrayList<String>();
+		Double currentDistanceTraveled;
+		Double currentDistanceTraveledLoaded;
+		Double currentDistanceTraveledUnloaded;
+		Double maxDistance = new Double(0.0);
+		Double maxDistanceLoaded = new Double(0.0);
+		Double maxDistanceUnloaded = new Double(0.0);
+		Integer numSegmentsOfMovement = new Integer(0);
+		Integer numSegmentsOfLoadedMovement = new Integer(0);
+		Integer numSegmentsOfUnloadedMovement = new Integer(0);
+		Boolean moving=false;
+		Boolean movingLoaded=false;
+		Boolean movingUnloaded=false;
+		
+		System.out.println(gson.toJson(cardToLift));
+
+		for (String truck : cardToLift.get(reportDriver).keySet()) {
+			totalTime=totalTime.plus(Duration.between(
+					cardToLift.get(reportDriver).get(truck).get(1),
+					cardToLift.get(reportDriver).get(truck).get(0)));
+		}
+		
+		System.out.println("The totalTime is "+totalTime);
 		
 		return null;
 	}
+	
+	/*************************************************************************
+	 * THESE ARE THE PREP ROUTINES WRITTEN BY CIHOLAS
+	 *************************************************************************/
 	
 	public void reportPrep(boolean debug) {
 		Gson gson = new Gson();
 
 		getMapsFromForkCsv();
-		if(debug) System.out.println("The palletMap is:" +gson.toJson(palletMap));
-		if(debug) System.out.println("The truckMap is:" +gson.toJson(truckMap));		
+		if(debug) System.out.println("The palletMap is:" 
+				+ gson.toJson(palletMap));
+		if(debug) System.out.println("The truckMap is:" 
+				+ gson.toJson(truckMap));		
 		
 		getValidDevs();
 		if(debug) System.out.println("Valid devices: "+gson.toJson(validDevs));
@@ -111,15 +177,17 @@ public class ProductivityReport implements Reportable {
 		if(debug) {
 			for(String dev:validDevs) {
 				if(positions.containsKey(dev)) 
-					System.out.println("    "+dev+": "+positions.get(dev).size());
+					System.out.println("    "+dev+": "
+							+ positions.get(dev).size());
 				else System.out.println("    "+dev+": 0");
 			}
 		}
 		
 		getDrivers();
 		
-		// So, card_time_pairs is a map between the driver card # and time entries
-		// ... and card_serial_pairs is a map between the card and the forklift devs;
+		// So, card_time_pairs is a map between the driver card # and time 
+		// entries ... and card_serial_pairs is a map between the card and 
+		// the forklift devs;
 		
 		// Now, it's time for:
 		// {
@@ -137,8 +205,10 @@ public class ProductivityReport implements Reportable {
 	
 	public void getMapsFromForkCsv() {
 		try {
-			File forkFile=new File(getClass().getClassLoader().getResource("forklift.csv").getFile());
-			BufferedReader csvReader = new BufferedReader(new FileReader(forkFile));
+			File forkFile=new File(getClass().getClassLoader().
+					getResource("forklift.csv").getFile());
+			BufferedReader csvReader = 
+					new BufferedReader(new FileReader(forkFile));
 			String row=csvReader.readLine();
 			while ((row = csvReader.readLine()) != null) {
 			    String[] data = row.split(",");
@@ -152,7 +222,8 @@ public class ProductivityReport implements Reportable {
 	
 	public void getValidDevs() {
 		List<String> availDevs = new ArrayList<String>();
-		String queryStr="show tag values from \"Position\" with key = \"Device\" "
+		String queryStr="show tag values from \"Position\" "
+				+ "with key = \"Device\" "
 				+ "where time >= "+start+" and time <= "+stop;
 		Query query = new Query(queryStr);
 		TimeUnit timeUnit = TimeUnit.NANOSECONDS;
@@ -166,7 +237,8 @@ public class ProductivityReport implements Reportable {
 				}
 			}
 		}
-		for(String dev : availDevs) if(truckMap.containsKey(dev)) validDevs.add(dev);
+		for(String dev : availDevs) 
+			if(truckMap.containsKey(dev)) validDevs.add(dev);
 	}
 	
 	public void getPositions() {
@@ -186,7 +258,8 @@ public class ProductivityReport implements Reportable {
 	}
 	
 	public void getDrivers() {
-		String cardQuery ="select \"Card ID\", \"Device\" from \"SpeedNforce V1\" where "
+		String cardQuery ="select \"Card ID\", \"Device\" "
+				+ "from \"SpeedNforce V1\" where "
 				+ "time >= %s and time <= %s and Device='%s'";
 		for (String dev : validDevs) {
 			String queryStr=String.format(cardQuery, start, stop, dev);
@@ -194,17 +267,21 @@ public class ProductivityReport implements Reportable {
 			TimeUnit timeUnit = TimeUnit.NANOSECONDS;
 			QueryResult r = influx.query(query, timeUnit);
 			InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
-			List<SpeedNForceV1> speedList=resultMapper.toPOJO(r, SpeedNForceV1.class);
+			List<SpeedNForceV1> speedList=
+					resultMapper.toPOJO(r, SpeedNForceV1.class);
 			for (SpeedNForceV1 entry : speedList) {
 				if(!drivers.containsKey(Integer.parseInt(entry.getCardId()))) {
-					LinkedList<SpeedNForceV1> myList = new LinkedList<SpeedNForceV1>();
+					LinkedList<SpeedNForceV1> myList = 
+							new LinkedList<SpeedNForceV1>();
 					myList.add(entry);
 					drivers.put(Integer.parseInt(entry.getCardId()),myList);
 				}
-				else drivers.get(Integer.parseInt(entry.getCardId())).add(entry);
+				else drivers.get(
+						Integer.parseInt(entry.getCardId())).add(entry);
 			}
 		}
-		//for (Integer key : drivers.keySet()) Collections.sort(drivers.get(key));
+		//for (Integer key : drivers.keySet()) 
+		//	Collections.sort(drivers.get(key));
 	}
 	
 	public void buildTimeMaps() {
@@ -213,15 +290,20 @@ public class ProductivityReport implements Reportable {
 			for(SpeedNForceV1 entry : entryList) {
 				
 				if(!cardToLift.containsKey(card)) {
-					HashMap<String,List<Instant>> myEntry = new HashMap<String,List<Instant>>();
-					myEntry.put(entry.getDevice(), Arrays.asList(entry.getTime(),entry.getTime()));
+					HashMap<String,List<Instant>> myEntry = 
+							new HashMap<String,List<Instant>>();
+					myEntry.put(entry.getDevice(), 
+							Arrays.asList(entry.getTime(),entry.getTime()));
 					cardToLift.put(card, myEntry);
 				}
 				else if(!cardToLift.get(card).containsKey(entry.getDevice())) {
-					cardToLift.get(card).put(entry.getDevice(), Arrays.asList(entry.getTime(),entry.getTime()));
+					cardToLift.get(card).put(entry.getDevice(), 
+							Arrays.asList(entry.getTime(),entry.getTime()));
 				} else {
-					Instant curStart = cardToLift.get(card).get(entry.getDevice()).get(0);
-					Instant curStop = cardToLift.get(card).get(entry.getDevice()).get(1);
+					Instant curStart = cardToLift.get(card).
+							get(entry.getDevice()).get(0);
+					Instant curStop = cardToLift.get(card).
+							get(entry.getDevice()).get(1);
 					Instant newTime = entry.getTime();
 					List<Instant> newTimes = new LinkedList<Instant>();
 					if(newTime.isBefore(curStart)) newTimes.add(newTime);
@@ -232,15 +314,20 @@ public class ProductivityReport implements Reportable {
 				}
 				
 				if(!liftToCard.containsKey(entry.getDevice())) {
-					HashMap<Integer,List<Instant>> myEntry = new HashMap<Integer,List<Instant>>();
-					myEntry.put(card, Arrays.asList(entry.getTime(),entry.getTime()));
+					HashMap<Integer,List<Instant>> myEntry = 
+							new HashMap<Integer,List<Instant>>();
+					myEntry.put(card, 
+							Arrays.asList(entry.getTime(),entry.getTime()));
 					liftToCard.put(entry.getDevice(), myEntry);
 				}
 				else if(!liftToCard.get(entry.getDevice()).containsKey(card)) {
-					liftToCard.get(entry.getDevice()).put(card, Arrays.asList(entry.getTime(),entry.getTime()));
+					liftToCard.get(entry.getDevice()).put(card, 
+							Arrays.asList(entry.getTime(),entry.getTime()));
 				} else {
-					Instant curStart = liftToCard.get(entry.getDevice()).get(card).get(0);
-					Instant curStop = liftToCard.get(entry.getDevice()).get(card).get(1);
+					Instant curStart = liftToCard.
+							get(entry.getDevice()).get(card).get(0);
+					Instant curStop = liftToCard.
+							get(entry.getDevice()).get(card).get(1);
 					Instant newTime = entry.getTime();
 					List<Instant> newTimes = new LinkedList<Instant>();
 					if(newTime.isBefore(curStart)) newTimes.add(newTime);
